668d5410ed1ca02139137ccd7c485b6210a5c1fa
Tseko Tsolov <tsekotsolov@gmail.com>
2018-05-19 19:37:11 +0300

    js standard added to package.json

--- 01.Node/Lab/index.js
+++ 01.Node/Lab/index.js
@@ -1,32 +1,27 @@
-let http = require('http');
-let port = 3000;
+let http = require('http')
+let port = 3000
 
 http.createServer((request, response) => {
-
   if (request.method === 'GET') {
-
     switch (request.url) {
-
       case '/':
-        response.write('Home');
-        response.end();
+        response.write('Home')
+        response.end()
 
-        break;
+        break
 
       case '/about':
-        response.write('About');
-        response.end();
+        response.write('About')
+        response.end()
 
-        break;
+        break
 
       default:
-        response.write('404: Not Found');
-        response.end();
-        break;
+        response.write('404: Not Found')
+        response.end()
+        break
     }
-
   }
+}).listen(port)
 
-}).listen(port);
-
-console.log(`Server listening on port ${port}...`);
+console.log(`Server listening on port ${port}...`)
