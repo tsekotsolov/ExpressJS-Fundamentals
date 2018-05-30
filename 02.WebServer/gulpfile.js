51d4640dd39198a0c7a1d0e2560e0f00e26adaf9
Tseko Tsolov <tsekotsolov@gmail.com>
2018-05-30 14:35:44 +0300

    up

---
+++ 02.WebServer/gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp')
+var htmlmin = require('gulp-htmlmin')
+
+gulp.task('minify', function () {
+  return gulp.src('./views/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/html'))
+})
+
+let cleanCSS = require('gulp-clean-css')
+
+gulp.task('minify-css', () => {
+  return gulp.src('./public/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('default', ['minify', 'minify-css'])
