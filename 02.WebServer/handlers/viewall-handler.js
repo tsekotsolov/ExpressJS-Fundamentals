b774c60803df665a4d05c9a6e3a12669418c4dd0
Tseko Tsolov <tsekotsolov@gmail.com>
2018-05-30 17:00:20 +0300

    movies are sorted

--- 02.WebServer/handlers/viewall-handler.js
+++ 02.WebServer/handlers/viewall-handler.js
@@ -11,6 +11,7 @@ module.exports = (request, response) => {
       const movieTemplate = `<div class="movie"><a href="/movies/{{my_id}}"><img class="moviePoster" src="{{Your movie poster URL}}"/></a></div>`
 
       let allMovies = request.db
+        .sort((a, b) => Number(a.movieYear) - Number(b.movieYear))
         .map(m => movieTemplate.replace('{{Your movie poster URL}}', decodeURIComponent(m.moviePoster)))
         .map((m, i) => m.replace('{{my_id}}', i))
         .join(' ')
