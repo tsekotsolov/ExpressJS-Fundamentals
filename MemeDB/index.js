e111fcd3b36a9689a921553eb28ec61c86492ae3
Tseko Tsolov <tsekotsolov@gmail.com>
2018-06-07 21:47:18 +0300

    updated index.js

--- MemeDB/index.js
+++ MemeDB/index.js
@@ -2,7 +2,7 @@ const http = require('http')
 const url = require('url')
 const handlers = require('./handlers/handlerBlender')
 const db = require('./config/dataBase')
-const port = 2323
+const port = process.env.PORT || 2323
 
 db.load().then(() => {
   console.log('testing')
