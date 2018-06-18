9d1d3a5ffbb923ff1f5f8417fe4c9aa71f10026a
Tseko Tsolov <tsekotsolov@gmail.com>
2018-06-18 23:00:22 +0300

    up

--- Article/handlers/searchHandler.js
+++ Article/handlers/searchHandler.js
@@ -1,20 +1,15 @@
 let Article = require('../models/ArticleSchema')
 module.exports = (req, res) => {
-  let keyword = req.body
-
-  console.log(keyword)
+  let keyword = req.body.keyword
 
   Article.find({}).then((allArticles) => {
     let arrayOfMatches = []
 
     for (const article of allArticles) {
-      if (article.title.includes(keyword.keyword)) {
+      if (article.title.includes(keyword)) {
         arrayOfMatches.push(article)
       }
     }
-
-    console.log(arrayOfMatches)
-
     res.render('search-results', {keyword, arrayOfMatches})
   })
 }
